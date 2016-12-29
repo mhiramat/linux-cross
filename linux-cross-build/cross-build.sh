26ecf534e5193af9c6be2199b56db5b3806f4093
Masami Hiramatsu <masami.hiramatsu@gmail.com>
2016-12-30 06:33:11 +0900

    Cleanup after build

--- linux-cross-build/cross-build.sh
+++ linux-cross-build/cross-build.sh
@@ -2,16 +2,18 @@
 
 set -ex
 
-mkdir -p /tmp/build
-ln -sf /opt/linux /tmp/build/source
 if [ ! -f /opt/linux/Makefile ]; then
   echo "Error: /opt/linux is not kernel source tree"
   exit 1
 fi
+
+mkdir -p /tmp/build
 cd /tmp/build
 
 for arch in $*; do
+  ln -sf /opt/linux /tmp/build/source
   echo "Building $arch kernel"
   make.cross ARCH=$arch defconfig
   make.cross ARCH=$arch
+  rm -rf ./*
 done
