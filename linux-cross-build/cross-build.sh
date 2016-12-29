b3b07da34da3b1e328cfa9719d590806c47bfe32
Masami Hiramatsu <masami.hiramatsu@gmail.com>
2016-12-29 18:15:01 +0900

    Fix to install some tools and forcibly mkdir/make symlink

--- linux-cross-build/cross-build.sh
+++ linux-cross-build/cross-build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-set -e
+set -ex
 
-mkdir /tmp/build
-ln -s /opt/linux /tmp/build/source
+mkdir -p /tmp/build
+ln -sf /opt/linux /tmp/build/source
 if [ ! -f /opt/linux/Makefile ]; then
   echo "Error: /opt/linux is not kernel source tree"
   exit 1
 fi
 cd /tmp/build
 
-for arch in $@; do
+for arch in $*; do
   echo "Building $arch kernel"
   make.cross ARCH=$arch defconfig
   make.cross ARCH=$arch
