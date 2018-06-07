4956221e24257dca5945e91a13766a272e3b9991
Masami Hiramatsu <masami.hiramatsu@gmail.com>
2018-06-07 15:03:15 +0900

    Show which arch build failed
    Show which arch build failed because usually the kernel
    build is too long to hold a log on console.

--- linux-cross-build/cross-build.sh
+++ linux-cross-build/cross-build.sh
@@ -10,10 +10,15 @@ fi
 mkdir -p /tmp/build
 cd /tmp/build
 
+eexit() { # message
+  echo $1
+  exit 1
+}
+
 for arch in $*; do
   ln -sf /opt/linux /tmp/build/source
   echo "Building $arch kernel"
   make.cross ARCH=$arch defconfig
-  make.cross ARCH=$arch
+  make.cross ARCH=$arch || eexit "Failed to build $arch kernel"
   rm -rf ./*
 done
